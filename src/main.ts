dd3a3ca8524d98392bfdc2dd0d996c24952ffa22
chen shun <3081876663@qq.com>
2026-08-02 15:18:36 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -31,8 +31,9 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:5173',
-      'http://localhost:3005',
+      'http://localhost:3000',
       'http://127.0.0.1:5173',
+      'http://localhost:3001',
     ],
     credentials: true,
   });

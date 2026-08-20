409c4221f665e4ec0ac3b4f32911f86cfdd6d876
chen shun <3081876663@qq.com>
2026-08-21 07:58:41 +0800

    1

--- src/modules/brand/brand.module.ts
+++ src/modules/brand/brand.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Brand } from './entities/brand.entity';
 import { Category } from '../categories/entities/category.entity';
+import { User } from '../users/entities/user.entity'; // ✅ 导入 User 实体
 import { BrandService } from './brand.service';
 import { BrandController } from './brand.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Brand, Category])],
+  imports: [TypeOrmModule.forFeature([Brand, Category, User])],
   controllers: [BrandController],
   providers: [BrandService],
   exports: [BrandService],
